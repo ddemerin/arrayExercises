7fb677e80a4174c9014a3ed6ae7cb1f493b85ae7
Daniel Demerin <ddemerin.code@gmail.com>
2020-10-31 11:15:32 -0400

    Exercise 1 functions with console.logs

--- exercises.js
+++ exercises.js
@@ -1,9 +1,28 @@
 let array1 = [1, 2, 3,];
 let array2 = ["jump", "duck", "punch"];
 
-function GetFirstElement(array){
-    console.log(array[0]);
+function GetFirstElement(array, index){
+    console.log(array[index]);
 }
 
-GetFirstElement(array1);
-GetFirstElement(array2);
+function RemoveLastElement(array){
+    console.log(array.pop());
+}
+
+function GetRandomElement(array){
+    console.log(array[Math.floor(Math.random() * array.length)]);
+}
+
+function AddElement(array, item){
+    array.push(item);
+    console.log(array);
+}
+
+
+GetFirstElement(array1, 0);
+GetFirstElement(array2, 0);
+RemoveLastElement(array1);
+RemoveLastElement(array2);
+GetRandomElement(array1);
+AddElement(array1, 3);
+AddElement(array2, "punch");
