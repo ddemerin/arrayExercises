7569d814383d292b230a18e3c54ffc55ce966582
Daniel Demerin <ddemerin.code@gmail.com>
2020-10-31 10:49:52 -0400

    Working on JS array exercises with students

---
+++ exercises.js
@@ -0,0 +1,9 @@
+let array1 = [1, 2, 3,];
+let array2 = ["jump", "duck", "punch"];
+
+function GetFirstElement(array){
+    console.log(array[0]);
+}
+
+GetFirstElement(array1);
+GetFirstElement(array2);
