db51f159e61733e326b824c6977778a05dd3a9ab
Daniel Demerin <ddemerin.code@gmail.com>
2020-10-31 11:27:58 -0400

    Showing how returns work within functions

--- exercises.js
+++ exercises.js
@@ -2,27 +2,40 @@ let array1 = [1, 2, 3,];
 let array2 = ["jump", "duck", "punch"];
 
 function GetFirstElement(array, index){
-    console.log(array[index]);
+    // console.log(array[index]);
+   return array[index];
 }
 
 function RemoveLastElement(array){
-    console.log(array.pop());
+    // console.log(array.pop());
+    return array.pop();
 }
 
 function GetRandomElement(array){
-    console.log(array[Math.floor(Math.random() * array.length)]);
+    // console.log(array[Math.floor(Math.random() * array.length)]);
+    return array[Math.floor(Math.random() * array.length)];
 }
 
 function AddElement(array, item){
     array.push(item);
-    console.log(array);
 }
 
-
-GetFirstElement(array1, 0);
-GetFirstElement(array2, 0);
-RemoveLastElement(array1);
-RemoveLastElement(array2);
-GetRandomElement(array1);
+let getFirstElement = GetFirstElement(array1, 0);
+let removeLastElement = RemoveLastElement(array1);
+let getRandomElement = GetRandomElement(array2);
 AddElement(array1, 3);
-AddElement(array2, "punch");
+AddElement(array1, 4)
+
+console.log(getFirstElement);
+console.log(removeLastElement);
+console.log(getRandomElement);
+console.log(array1);
+
+
+// GetFirstElement(array1, 0);
+// GetFirstElement(array2, 0);
+// RemoveLastElement(array1);
+// RemoveLastElement(array2);
+// GetRandomElement(array1);
+// AddElement(array1, 3);
+// AddElement(array2, "punch");
